5ef65089a51bd73ba4f3080fc67e350f01f63bb0
Cyril C <cyrilc_mail-github@yahoo.fr>
2022-12-21 14:03:56 +0000

    star 1

--- 01/main.go
+++ 01/main.go
@@ -1,14 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	b, err := os.ReadFile("./input.txt")
+	readFile, err := os.Open("./input.txt")
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println(string(b))
+	defer readFile.Close()
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+	maxAmount := 0
+	amounts := make([]int, 1)
+	currentElf := 0
+	for fileScanner.Scan() {
+		s := fileScanner.Text()
+		if s == "" {
+			if amounts[currentElf] > maxAmount {
+				maxAmount = amounts[currentElf]
+			}
+			currentElf++
+			amounts = append(amounts, 0)
+		} else {
+			v, err := strconv.Atoi(s)
+			if err != nil {
+				panic(err)
+			}
+			amounts[currentElf] += v
+		}
+	}
+	fmt.Println(maxAmount)
 }
